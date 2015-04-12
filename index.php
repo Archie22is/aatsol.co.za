980d107e14d983458ebbe1cff9e1ef9bb9f34c06
Archie Makuwa <archie@aatsol.co.za>
2015-04-12 16:28:33 +0200

    New aatsol.co.za commit on a new repo

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Archie Makuwa(@aatsol) - a Developer, an Entrepreneur and Internet Citizen</title>
+    <meta name="description" content="Archie Makuwa(@aatsol) - a Developer, an Entrepreneur and Internet Citizen">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link id="page_favicon" href="/favicon.ico" rel="icon" type="image/x-icon" />
+    <script type="text/javascript">
+        addEventListener("load", function(event)
+        {
+            setTimeout(function(){window.scrollTo(0, 1);}, 100);
+        }, false);
+    </script>
+</head>
+
+
+<body>
+    <header>
+        <div class="container">
+            <div class="title">
+                <img src="images/archie.jpg" alt="Archie Makuwa (aatsol)" title="Archie Makuwa (aatsol)" />
+                <h1>Hi, I'm Archie.</h1>
+                <p>I'm Software Developer, Dntrepreneur &amp;
+                    <a href="http://en.wikipedia.org/wiki/Random_Hacks_of_Kindness#Featured_hacks" target="_blank">
+                        RHoKer
+                    </a>.
+                    <br>
+                    I'm currently lead a great bunch of developers at <a href="http://www.struto.co.uk" target="_blank">
+                        Struto</a> &amp; I write code for <a href="http://www.thebillplease.co.za/">TheBillPlease</a>.
+                <p>
+                <p>You can connect with me on the following platforms:</p>
+                <ul class="social-icons">
+                    <li>
+                        <a href="http://www.linkedin.com/profile/view?id=121157532" target="_blank" title="My LinkedIn Profile">
+                            <i class="fa fa-linkedin-square"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Archie22is" target="_blank" title="My Github Profile">
+                            <i class="fa fa-github-square"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://bitbucket.org/Archie22is" target="_blank" title="My BitBucket Profile">
+                            <i class="fa fa-bitbucket-square"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/aatsol" target="_blank" title="My Twitter Profile">
+                            <i class="fa fa-twitter-square"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://wordpress.org/support/profile/archie22is" target="_blank" title="My WordPress profile">
+                            <i class="fa fa-wordpress"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:archie@aatsol.co.za" title="My Email Address">
+                            <i class="fa fa-envelope-square"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <section class="location">
+        <div class="container">
+            <div class="sa">
+                <img src="images/sa.png" alt="South Africa">
+                <div class="beacon"></div>
+                <div class="pulse"></div>
+            </div>
+            <p>I live at the tip of <a href="https://en.wikipedia.org/wiki/Africa" target="_blank">Africa</a>
+                in the beautiful city of
+                <a href="http://en.wikipedia.org/wiki/Cape_Town" target="_blank">Cape Town</a>.
+            </p>
+        </div>
+    </section>
+
+
+    <!-- Professional Work -->
+    <section class="professional-work">
+        <div class="container">
+            <div class="inner-container">
+                <h2>Hire Me!</h2>
+                <p>I love collaborating on projects and I love coffee! If you like my work and would like to meet over a cup of coffee to see how I can work with you and/or vice versa, please get in touch :-)
+                </p>
+            </div>
+        </div>
+        <div class="container">
+
+        </div>
+    </section>
+
+
+    <!-- Some javascript -->
+    <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+    <!--
+    <script type="text/javascript" src="js/jquery.migrate.min.js"></script>
+    <script type="text/javascript" src="js/slick.min.js"></script>
+    <script type="text/javascript" src="js/snowstorm-min.js"></script>
+    -->
+    <script type="text/javascript" src="js/script.js"></script>
+
+    <!-- Google Analytics -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-34095081-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
+    <footer>
+        <div class="container">
+            <p id="credits">
+                This site is proudly hosted on 
+                <a href="https://www.digitalocean.com/?refcode=c816b0a1a758" target="_blank">Digital Ocean</a>.
+                View source on 
+                <a href="https://github.com/Archie22is" target="_blank">Github</a></p>
+        </div>
+    </footer>
+    </body>
+</html>
